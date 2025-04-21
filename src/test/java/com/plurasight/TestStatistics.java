127178bde657cf629709ae210b43ade0d6815d0f
emvherrera <emilyvherrera98@gmail.com>
2025-04-21 15:51:12 -0400

    Commit TestStatistics

---
+++ src/test/java/com/plurasight/TestStatistics.java
@@ -0,0 +1,16 @@
+package com.plurasight;
+
+public class TestStatistics {
+    public static void main(String[] args) {
+
+        int[] scores = {80, 85, 90, 92, 94, 97, 98, 100};
+
+        for (int i = 0; i < scores.length; i++) {
+            System.out.println(scores[i] + " ");
+        }
+//    for (int i = 0; i < scores.length; i++) {
+//        System.out.print(scores[i] + " ");
+//
+//    }
+    }
+}
